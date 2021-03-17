6429ff68b58699bb53faba5c96964b9ed604ec6d
Flavio Silva <flavioms86@hotmail.com>
2021-03-17 09:23:38 -0300

    Content Provider - Complementando cardview com constrainLayout

--- app/src/main/java/com/example/contentproviderdio/MainActivity.kt
+++ app/src/main/java/com/example/contentproviderdio/MainActivity.kt
@@ -6,6 +6,7 @@ import android.os.Bundle
 import androidx.loader.app.LoaderManager
 import androidx.loader.content.CursorLoader
 import androidx.loader.content.Loader
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.contentproviderdio.database.NotesDatabaseHelper.Companion.TITLE_NOTES
 import com.example.contentproviderdio.database.NotesProvider.Companion.URI_NOTES
@@ -16,6 +17,8 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
     lateinit var noteRecyclerView: RecyclerView
     lateinit var noteAdd: FloatingActionButton
 
+    lateinit var adapter: NotesAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -23,13 +26,18 @@ class MainActivity : AppCompatActivity(), LoaderManager.LoaderCallbacks<Cursor> 
         noteAdd = findViewById(R.id.note_add)
         noteAdd.setOnClickListener {  }
 
+        adapter = NotesAdapter()
+        adapter.setHasStableIds(true)
+
         noteRecyclerView = findViewById(R.id.notes_recycler)
+        noteRecyclerView.layoutManager = LinearLayoutManager(this)
+        noteRecyclerView.adapter = adapter
     }
 
     //Instancia o que sera buscado - pesquisa no content provider
-    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> {
+    override fun onCreateLoader(id: Int, args: Bundle?): Loader<Cursor> =
         CursorLoader(this, URI_NOTES, null, null, null, TITLE_NOTES)
-    }
+
     //pegar os dados recebidos para manipulação
     override fun onLoadFinished(loader: Loader<Cursor>, data: Cursor?) {
         if (data != null) {}
